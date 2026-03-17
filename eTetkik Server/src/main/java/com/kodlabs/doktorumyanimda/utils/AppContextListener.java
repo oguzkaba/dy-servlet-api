426e783645a603fb38c5c88b95452461f2df5320
oguzkaba <oguz-kaba@hotmail.com>
2026-03-17 10:15:55 +0300

    fix

---
+++ eTetkik Server/src/main/java/com/kodlabs/doktorumyanimda/utils/AppContextListener.java
@@ -0,0 +1,18 @@
+package com.kodlabs.doktorumyanimda.utils;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Common.init(sce.getServletContext());
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+}
